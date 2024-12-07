6b54864837e3d23a766b643ad8b9a76c4b58151a
K1amens <108186216+K1amens@users.noreply.github.com>
2024-12-07 17:05:31 +0300

    module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -4,17 +4,18 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
     )):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
+
     elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
-        return
+
 
     elif sender == "university.help@gmail.com":
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
-        return
+
 
     elif sender != "university.help@gmail.com":
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
-        return
+
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
